acf0c16a5fae70fadadcff24da15586b6ff36cd7
Chris <43553799+JBMILHOUSE@users.noreply.github.com>
2021-03-31 13:08:45 -0300

    inicializando aplicação

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+/**
+ * GET => Buscar
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração especifica
+ */
+
+
+ 
+app.listen(3333, () => console.log("Server is running"));
